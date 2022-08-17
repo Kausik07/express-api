0d9bcf1e5b401c2c4854b4407b1833b0bb48fc63
kausik07 <kausikpattanaik2002@gmail.com>
2022-08-18 00:39:19 +0530

    base config

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const { PORT } = require('./configs');
+const router = require('./routers');
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors())
+app.use(helmet())
+app.use(morgan("dev"))
+
+app.get("/health", (req, res) => {
+    res.status(200).send('OK');
+})
+
+console.log('☄', 'Base Route', '/api');
+app.use('/api', router);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
---
+++ src/configs/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "MONGODB_URI": "mongodb://kusik:xxxxxxxxxxxxxxxxxxx@localhost:27017/express-practice",
+    "PORT": 4000
+};
